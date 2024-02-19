2be8d4e6e00857cd15a1b4efd1b6ff82edaae2ec
Seiya Kumada <seiya.kumada@gmail.com>
2024-02-19 06:36:50 +0000

    binaryデータを読み込む関数を実装した。

--- src/scene/colmap_loader.cpp
+++ src/scene/colmap_loader.cpp
@@ -2,17 +2,18 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
-
+#include <variant>
+#include <cstring>
 Image::Image() {}
 
 Image::Image(
-    uint64_t id,
+    int id,
     const std::array<double, 4> &qvec,
     const std::array<double, 3> &tvec,
     int camera_id,
     const std::string &name,
     const std::vector<std::pair<double, double>> &xys,
-    const std::vector<int> &point3D_ids)
+    const std::vector<int64_t> &point3D_ids)
     : id_{id},
       qvec_{qvec},
       tvec_{tvec},
@@ -26,53 +27,141 @@ Image::Image(
 namespace
 {
     template <typename T>
-    T readBinary(std::ifstream &stream)
+    T read_data_core(const char *&cursor)
     {
-        T data;
-        stream.read(reinterpret_cast<char *>(&data), sizeof(T));
-        return data;
+        T value;
+        std::memcpy(&value, cursor, sizeof(T));
+        cursor += sizeof(T);
+        return value;
     }
+    // データ型を格納するためのvariant
+    using DataVariant = std::variant<int, double, uint64_t, char, int64_t>;
+
+    // フォーマットに従ってデータを読み込む関数
+    std::vector<DataVariant> read_data(const char *data, const std::string &format_char_sequence)
+    {
+        std::vector<DataVariant> result;
+        const char *cursor = data;
+        for (char fmt : format_char_sequence)
+        {
+            switch (fmt)
+            {
+            case 'i':
+            { // 整数
+                auto value = read_data_core<int>(cursor);
+                result.push_back(value);
+                break;
+            }
+            case 'd':
+            { // 倍精度浮動小数点数
+                auto value = read_data_core<double>(cursor);
+                result.push_back(value);
+                break;
+            }
+            case 'Q':
+            { // uint64_t
+                auto value = read_data_core<uint64_t>(cursor);
+                result.push_back(value);
+                break;
+            }
+            case 'c':
+            { // char
+                auto value = read_data_core<char>(cursor);
+                result.push_back(value);
+                break;
+            }
+            case 'q':
+            { // long long
+                auto value = read_data_core<int64_t>(cursor);
+                result.push_back(value);
+                break;
+            }
+            // 他のフォーマット文字に対応する場合は、ここにケースを追加
+            default:
+                // 未知のフォーマット文字の処理
+                std::cerr << "Unsupported format character: " << fmt << std::endl;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    auto read_next_bytes(
+        std::ifstream &stream,
+        int num_bytes,
+        const std::string &format_char_sequence) -> std::vector<DataVariant>
+    {
+        auto data = std::vector<char>(num_bytes);
+        stream.read(data.data(), num_bytes);
+        return read_data(data.data(), format_char_sequence);
+    }
+
+    template <typename T>
+    std::vector<T> sliceEveryThirdElement(const std::vector<T> &input, size_t start, size_t interval)
+    {
+        std::vector<T> result;
+        // 開始位置が入力ベクタのサイズ以上の場合、空のベクタを返す
+        if (start >= input.size())
+        {
+            return result;
+        }
+        for (size_t i = start; i < input.size(); i += interval)
+        {
+            result.push_back(input[i]);
+        }
+        return result;
+    }
+
 }
 
 auto read_extrinsics_binary(const std::string &path_to_model_file) -> std::map<uint64_t, Image>
 {
-    std::ifstream file(path_to_model_file, std::ios::binary);
-    if (!file.is_open())
+    std::ifstream fid(path_to_model_file, std::ios::binary);
+    if (!fid.is_open())
     {
         throw std::runtime_error("Unable to open file: " + path_to_model_file);
     }
 
     std::map<uint64_t, Image> images;
-    uint64_t num_reg_images = readBinary<uint64_t>(file);
-    std::cout << "num_reg_images: " << num_reg_images << std::endl;
+    uint64_t num_reg_images = std::get<uint64_t>(read_next_bytes(fid, 8, "Q")[0]);
+    // std::cout << "_/_/_/num_reg_images:" << num_reg_images << std::endl;
     for (uint64_t i = 0; i < num_reg_images; ++i)
     {
-        std::cout << i << std::endl;
-        uint64_t image_id = readBinary<uint64_t>(file);
-        std::array<double, 4> qvec;
-        for (int j = 0; j < 4; ++j)
-            qvec[j] = readBinary<double>(file);
-        std::array<double, 3> tvec;
-        for (int j = 0; j < 3; ++j)
-            tvec[j] = readBinary<double>(file);
-        int camera_id = readBinary<int>(file);
-        std::string image_name;
-        char current_char;
-        while (true)
+        auto binary_image_properties = read_next_bytes(fid, 64, "idddddddi");
+        auto image_id = std::get<int>(binary_image_properties[0]);
+        auto qvec = std::array<double, 4>{
+            std::get<double>(binary_image_properties[1]),
+            std::get<double>(binary_image_properties[2]),
+            std::get<double>(binary_image_properties[3]),
+            std::get<double>(binary_image_properties[4])};
+
+        auto tvec = std::array<double, 3>{
+            std::get<double>(binary_image_properties[5]),
+            std::get<double>(binary_image_properties[6]),
+            std::get<double>(binary_image_properties[7])};
+        auto camera_id = std::get<int>(binary_image_properties[8]);
+        auto current_char = std::get<char>(read_next_bytes(fid, 1, "c")[0]);
+        // std::cout << "_/_/_/image_id:" << image_id << std::endl;
+        // std::cout << "_/_/_/camera_id:" << camera_id << std::endl;
+        auto image_name = std::string{};
+        while (current_char != '\0')
         {
-            file.read(&current_char, 1);
-            if (current_char == '\0')
-                break;
             image_name += current_char;
+            current_char = std::get<char>(read_next_bytes(fid, 1, "c")[0]);
         }
-        uint64_t num_points2D = readBinary<uint64_t>(file);
+        // std::cout << "_/_/_/image_name:" << image_name << std::endl;
+        uint64_t num_points2D = std::get<uint64_t>(read_next_bytes(fid, 8, "Q")[0]);
+        // std::cout << "_/_/_/num_points2D:" << num_points2D << std::endl;
+
         std::vector<std::pair<double, double>> xys;
-        std::vector<int> point3D_ids;
+        std::vector<int64_t> point3D_ids;
         for (uint64_t j = 0; j < num_points2D; ++j)
         {
-            double x = readBinary<double>(file);
-            double y = readBinary<double>(file);
-            int point_id = readBinary<int>(file);
+            auto x_y_id_s = read_next_bytes(fid, 24, "ddq");
+            double x = std::get<double>(x_y_id_s[0]);
+            double y = std::get<double>(x_y_id_s[1]);
+            int64_t point_id = std::get<int64_t>(x_y_id_s[2]);
             xys.emplace_back(x, y);
             point3D_ids.push_back(point_id);
         }
--- src/scene/colmap_loader.h
+++ src/scene/colmap_loader.h
@@ -5,23 +5,23 @@
 
 struct Image
 {
-    uint64_t id_;
+    int id_;
     std::array<double, 4> qvec_;
     std::array<double, 3> tvec_;
     int camera_id_;
     std::string name_;
     std::vector<std::pair<double, double>> xys_;
-    std::vector<int> point3D_ids_;
+    std::vector<int64_t> point3D_ids_;
 
     Image();
     Image(
-        uint64_t id,
+        int id,
         const std::array<double, 4> &qvec,
         const std::array<double, 3> &tvec,
         int camera_id,
         const std::string &name,
         const std::vector<std::pair<double, double>> &xys,
-        const std::vector<int> &point3D_ids);
+        const std::vector<int64_t> &point3D_ids);
 };
 
 auto read_extrinsics_binary(const std::string &path_to_model_file) -> std::map<uint64_t, Image>;
